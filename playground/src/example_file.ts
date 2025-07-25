bd2fa94bb05dc283ef83913d70cf2f801f624ebd
Alexander Heimbuch <alex@osdyne.com>
2025-07-25 15:44:12 +0200

    tracing_ui: resolve merge conflicts

--- playground/src/example_file.ts
+++ playground/src/example_file.ts
@@ -1,13 +1,15 @@
-const app = document.getElementById("app");
-
-window.addEventListener("load", () => {
-(window as any)
-  .loadPerfetto(app, { rootUrl: '/perfetto/', initialState: { sidebarVisible: false, showFileHandling: false } })
-  .then((app) => {
-    fetch('assets/data/trace.pb')
-      .then((res) => res.arrayBuffer())
-      .then((buffer) => {
-        app.openTraceFromBuffer({ buffer, title: 'File' });
-      });
-  });
+window.addEventListener('load', () => {
+  const app = document.getElementById('app');
+  (window as any)
+    .loadPerfetto(app, {
+      rootUrl: '/perfetto/',
+      initialState: { sidebarVisible: false, showFileHandling: false }
+    })
+    .then((app) => {
+      fetch('assets/data/trace.pb')
+        .then((res) => res.arrayBuffer())
+        .then((buffer) => {
+          app.openTraceFromBuffer({ buffer, title: 'File' });
+        });
+    });
 });
--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,5 +1,7 @@
-window.addEventListener("load", () => {
-  (window as any).loadPerfetto({
+
+window.addEventListener('load', () => {
+  const container = document.getElementById('app');
+  (window as any).loadPerfetto(container, {
     rootUrl: '/perfetto/',
     initialState: { sidebarVisible: true, showFileHandling: true }
   });
